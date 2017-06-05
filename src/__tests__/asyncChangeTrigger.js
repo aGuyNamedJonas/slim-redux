71118ba94a75c9293a10d7c7a94cab43b440ea1b
jpeeck <jonas.peeck@bild.de>
2017-06-05 22:37:18 +0200

    WIP: Implementing asyncChangeTrigger()

--- src/__tests__/asyncChangeTrigger.js
+++ src/__tests__/asyncChangeTrigger.js
@@ -12,7 +12,7 @@ describe('Async change triggers', () => {
   describe('asyncChangeTrigger() (regular / default case)', () => {
     test('returns a function', () => expect(
       isFunction(
-        asyncChangeTrigger({ noopCt }, state => state)
+        asyncChangeTrigger([ noopCt ], (noopCt, state) => state)
       )
     ).toBe(true));
 
@@ -21,7 +21,7 @@ describe('Async change triggers', () => {
       const reducer  = sinon.spy(state => state),
             altStore = createSlimReduxStore({ alt: 'altStore' }),
             spyCt    = changeTrigger('NOOP', reducer),
-            act      = asyncChangeTrigger({ spyCt }, state => spyCt(), altStore);
+            act      = asyncChangeTrigger([ spyCt ], (spyCt, state) => spyCt(), altStore);
 
       act();
       expect(cbFunc.calledWith(altStore.getState())).toBe(true);
@@ -29,34 +29,42 @@ describe('Async change triggers', () => {
   });
 
   describe('asyncChangeTrigger() (error cases)', () => {
-    test('throws when more than three arguments are provided', () =>
-      expect(() => asyncChangeTrigger({ noopCt }, state => state, store, 'ILLEGAL FOURTH ARGUMENT')).toThrow();
-    );
+    test('throws when more than three arguments are provided', () => {
+      expect(() => asyncChangeTrigger([ noopCt ], (noopCt, state) => state, store, 'ILLEGAL FOURTH ARGUMENT')).toThrow();
+    });
 
-    test('throws when change triggers (first argument) is not an object', () =>
-      expect(() => asyncChangeTrigger('NOT AN OBJECT', state => state)).toThrow();
-    );
+    test('throws when change triggers (first argument) is not an array', () => {
+      expect(() => asyncChangeTrigger('NOT AN ARRAY', state => state)).toThrow();
+    });
 
     test('throws when change triggers (first argument) is null or undefined', () => {
       expect(() => asyncChangeTrigger(null, state => state)).toThrow();
       expect(() => asyncChangeTrigger(undefined, state => state)).toThrow();
     });
 
-    test('throws when triggerFunction (second argument) is not a function', () =>
-      expect(() => asyncChangeTrigger({ noopCt }, 'NOT A FUNCTION')).toThrow();
-    );
+    test('throws when triggerFunction (second argument) is not a function', () => {
+      expect(() => asyncChangeTrigger([ noopCt ], 'NOT A FUNCTION')).toThrow();
+    });
 
-    test('throws when triggerFunction (second argument) has zero arguments', () =>
-      expect(() => asyncChangeTrigger({ noopCt }, () => {})).toThrow();
-    );
+    test('throws when triggerFunction (second argument) is null or undefined', () => {
+      expect(() => asyncChangeTrigger([ noopCt ], null)).toThrow();
+      expect(() => asyncChangeTrigger([ noopCt ], undefined)).toThrow();
+    });
 
-    test('throws when triggerFunction (second argument) does not have "state" as its last argument', () =>
-      expect(() => asyncChangeTrigger({ noopCt }, () => {})).toThrow();
-    );
+    test('throws when triggerFunction (second argument) does not have #change-triggers arguments (or +1 for state)', () => {
+      // This is allowed - it can have one more argument than change triggers if the last one is "state"
+      expect(() => asyncChangeTrigger([ noopCt ], (noopCt, state) => {})).not.toThrow();
 
-    test('throws when the third argument provided is not a slim-redux instance', () =>
-        expect(() => asyncChangeTrigger({ noopCt }, state => state, 'NOT A SLIM-REDUX STORE INSTANCE')).toThrow();
-    );
+      // Too little arguments (needs to have the change trigger in the arguments)
+      expect(() => asyncChangeTrigger([ noopCt ], () => {})).toThrow();
+
+      // Too many arguments
+      expect(() => asyncChangeTrigger([ noopCt ], (too, many, args) => {})).toThrow();
+    });
+
+    test('throws when the third argument provided is not a slim-redux instance', () => {
+        expect(() => asyncChangeTrigger([ noopCt ], (noopCt, state) => state, 'NOT A SLIM-REDUX STORE INSTANCE')).toThrow();
+    });
   });
 
   describe('asyncChangeTrigger() returned function', () => {
--- src/asyncChangeTrigger.js
+++ src/asyncChangeTrigger.js
@@ -1,3 +1,56 @@
-const dummyPlaceHolderReturnFunction = () => {}
+import { error as _err, getType, isArray, isSet, isFunction, getFuncParamNames, isSlimReduxStore } from './util';
 
-export const asyncChangeTrigger = () => dummyPlaceHolderReturnFunction
+export function asyncChangeTrigger(changeTriggers, triggerFunction, storeArg) {
+  const error = msg => _err('asyncChangeTrigger()', msg);
+
+  /*
+    Parameter validation (see tests)
+  */
+  if(arguments.length > 3)
+    error(`Only 3 arguments allowed, ${arguments.length} given: \n ${JSON.stringify(arguments, null, 2)}`);
+
+  // Check changeTriggers
+  if(!isArray(changeTriggers))
+    error(`"changeTriggers" (first argument) needs to be an array, got ${getType(changeTriggers)} instead: \n ${JSON.stringify(arguments, null, 2)}`);
+
+  if(!isSet(changeTriggers))
+    error(`"changeTriggers" (first argument) cannot be null, empty, or undefined: \n ${JSON.stringify(arguments, null, 2)}`);
+
+  // Check triggerFunction
+  if(!isFunction(triggerFunction))
+    error(`"triggerFunction" (second argument) needs to be a function, got ${getType(triggerFunction)} instead: \n ${JSON.stringify(arguments, null, 2)}`);
+
+  if(!isSet(triggerFunction))
+    error(`"triggerFunction" (second argument) cannot be null or undefined: \n ${JSON.stringify(arguments, null, 2)}`);
+
+  const argNames    = getFuncParamNames(triggerFunction),
+        numberArgs  = argNames.length,
+        numberCTs   = changeTriggers.length,
+        hasStateArg = argNames[numberArgs - 1] === 'state';
+
+  if(!hasStateArg && numberArgs > numberCTs)
+    error(`"triggerFunction" (second argument) cannot have more arguments (=${numberArgs} given) than amount of change triggers (=${numberCTs} allowed), unless the last argument is "state" (does NOT seem to be the case): \n ${JSON.stringify(arguments, null, 2)}`);
+
+  if(hasStateArg && numberArgs > numberCTs + 1)
+    error(`"triggerFunction" (second argument) cannot have more arguments (=${numberArgs} given) than amount of change triggers (=${numberCTs} + allowed), unless the last argument is "state" (DOES seem to be the case here): \n ${JSON.stringify(arguments, null, 2)}`);
+
+  if(numberArgs < numberCTs)
+    error(`"triggerFunction" (second argument) needs to have at least as many arguments as amount of change triggers passed in (given: ${numberArgs}, expected: ${numberCTs}):  \n ${JSON.stringify(arguments, null, 2)}`);
+
+  // Check (optional) store instance
+  if(storeArg && !isSlimReduxStore(storeArg))
+    error(`"storeArg" (third argument) is optional, but has to be a slim-redux store instance if provided: \n${JSON.stringify(arguments, null, 2)}`);
+
+  const store = storeArg || window.store;
+
+  if(!store)
+    error(`No store instance provided in global and local scope! In case you set "disableGlobalStore" when creating slim-redux store, make sure to pass it in as the last argument!\n ${JSON.stringify(arguments, null, 2)}`);
+
+  if(!isSlimReduxStore(store))
+    error(`Store instance provided is not a slim-redux store! \n ${JSON.stringify(arguments, null, 2)}`);
+
+  /*
+    Implementation
+  */
+  // TODO :)
+}
