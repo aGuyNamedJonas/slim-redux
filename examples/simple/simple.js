0b3e8ea0dab077ef574ee1084ca7f0cc7053a170
jpeeck <jonas.peeck@bild.de>
2017-06-16 21:12:25 +0200

    Finished simple example

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -3,13 +3,13 @@ import { createSlimReduxStore, subscription, calculation, changeTrigger, asyncCh
 const store                = createSlimReduxStore({ counter: 0 }),
       counterSub           = subscription('state.counter', counter => console.log(`New counter state: ${counter}`)),
       counterTimesTen      = calculation(['state.counter'], counter => counter * 10, incCounter => console.log(`Increased calculated counter state: ${incCounter}`)),
-      increaseCounter      = changeTrigger('INCREASE_COUNTER', (state) => ({ counter: state.counter + 1 })),
-      increaseCounterAsync = asyncChangeTrigger({ increaseCounter }, () => setTimeout(() => increaseCounter(), 2000) );
+      increaseCounter      = changeTrigger('INCREASE_COUNTER', (inc, state) => ({ counter: state.counter + inc })),
+      increaseCounterAsync = asyncChangeTrigger({ increaseCounter }, () => setTimeout(() => increaseCounter(1), 2000) );
 
 // All synchronous
-increaseCounter();
-increaseCounter();
-increaseCounter();
+increaseCounter(1);
+increaseCounter(2);
+increaseCounter(3);
 
 // Asynchronous change trigger
 console.log('Increasing counter in 2000ms...');
